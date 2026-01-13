a42f28dae667e1209f435861dbdec6749a575054
nadha-rinsad12 <147365907+nadha-rinsad12@users.noreply.github.com>
2026-01-13 15:22:39 +0530

    Add files via upload

---
+++ Js/home.js
@@ -0,0 +1,14 @@
+// Initialization for ES Users
+import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown, Collapse });
+
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
+
+// Initialization for ES Users
+import { Dropdown, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown });
